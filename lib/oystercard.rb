7b1931390fb184dabd57187e0daf59accc9a4cd6
Chris Walsh <highwalshy@gmail.com>
2020-11-03 15:43:49 +0000

    Closing #8: added the touch_in, touch_out and in_journey? methods along with tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,10 +5,28 @@ MAX_BALANCE = 90
 
   def initialize
     @balance = 0
+    @state = false
   end
 
   def top_up(n)
     raise "No, spend some first." if balance + n > MAX_BALANCE
     @balance += n
+  end
+
+  def deduct(n)
+    raise "No monies." if balance - n <= 0
+    @balance -= n
+  end
+
+  def touch_in
+    @state = true
+  end
+
+  def in_journey?
+    @state
+  end
+
+  def touch_out
+    @state = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,7 +12,31 @@ describe Oystercard do
     end
     it "can't be topped up more than £90" do
       limit = Oystercard::MAX_BALANCE
-      expect { subject.top_up(limit + 1) }.to raise_error 'No, spend some first.' 
+      expect { subject.top_up(limit + 1) }.to raise_error 'No, spend some first.'
+    end
+  end
+
+  context "it has a defined state while in use" do
+    it "responds to the touch_in method" do
+      expect(subject).to respond_to :touch_in
+    end
+    it "starts their journey when touch_in is called"do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+    it "responds to the in_journey method" do
+      expect(subject).to respond_to :in_journey?
+    end
+    it "knows that it is not in a journey unless it has been touched in" do
+      expect(subject.in_journey?).to eq(false)
+    end
+    it "responds to the touch_out method" do
+      expect(subject).to respond_to :touch_out
+    end
+    it "knows that when it has touched out, it is no longer in journey" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
     end
   end
 end
